07cda32297eb3927482cc96de39910260234a13a
Abhishek Roy <royabhishek77@gmail.com>
2024-11-23 16:40:39 +0000

    convolve with kernel size=3. benchmarked with naive implementation

--- pulp/examples/1d_conv.rs
+++ pulp/examples/1d_conv.rs
@@ -1,131 +1,171 @@
 use pulp::{Arch, Simd, WithSimd};
-use reborrow::IntoConst;
+use diol::prelude::*;
+use aligned_vec::avec;
+const KERNEL_RECIP_3: f32 = 1.0 / 3.0;
 
-struct Convolver<'a> {
+struct Convolver3k<'a> {
     input: &'a [f32],
-    kernel: &'a [f32],
+    kernel: &'a [f32; 3],
     output: &'a mut [f32],
 }
 
-impl<'a> WithSimd for Convolver<'a> {
+struct TransposedHeadsIter<'a, S: Simd> {
+    heads: [&'a [S::f32s]; 3],
+    current_idx: usize,
+}
+
+impl<'a, S: Simd> Iterator for TransposedHeadsIter<'a, S> {
+    type Item = [S::f32s; 3];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current_idx >= self.heads[0].len() {
+            return None;
+        }
+        let result = [
+            self.heads[0][self.current_idx],
+            self.heads[1][self.current_idx],
+            self.heads[2][self.current_idx],
+        ];
+        self.current_idx += 1;
+        Some(result)
+    }
+}
+
+impl<'a> WithSimd for Convolver3k<'a> {
     type Output = ();
 
     #[inline(always)]
     fn with_simd<S: Simd>(self, simd: S) -> Self::Output {
-
         // assert that kernal length is less than 16
-        let kernel_len = self.kernel.len();
-        assert!(self.kernel.len() < 10);
-        // assert that input length is greater than kernel length
-        assert!(self.input.len() >= self.kernel.len());
-
+        assert_eq!(self.kernel.len(), 3);
+        assert!(self.kernel.len() < 4);
         // assert that input is same length as output
         assert!(self.input.len() == self.output.len());
-
         // create a simd vector of the reciprocal of the kernel length
-        let kernel_len_recip_simd = simd.splat_f32s(1.0 / kernel_len as f32);
-        
-        // make a vec to store each of the conv kernel elements as simd vectors
-        let kernel_simd: Vec<_> = self.kernel.iter().map(|x| simd.splat_f32s(*x)).collect();
-
+        let kernel_len_recip_simd = simd.splat_f32s(KERNEL_RECIP_3);
+        let kernel_simd: [S::f32s; 3] = [
+            simd.splat_f32s(self.kernel[0]),
+            simd.splat_f32s(self.kernel[1]),
+            simd.splat_f32s(self.kernel[2]),
+        ];
         // create a vec of shifted copies of the input
         // shape [shift,len(input)]
-        let shifted_copies = create_shifted_copies(self.input, &kernel_len, &0.0);
-        let (shifted_heads, _shifted_tails): (Vec<_>, Vec<_>) = shifted_copies
-            .iter().map(|v| S::as_simd_f32s(&v)).unzip();
+        let shifted_copies = create_3_shifted_copies(self.input);
+        let shifted_splits = [
+            S::as_simd_f32s(shifted_copies[0]),
+            S::as_simd_f32s(shifted_copies[1]),
+            S::as_simd_f32s(shifted_copies[2]),
+        ];
 
-        let shifted_heads_t  = transpose_heads(simd, shifted_heads);
+        let shifted_heads = [
+            shifted_splits[0].0,
+            shifted_splits[1].0,
+            shifted_splits[2].0,
+        ];
+
+        let shifted_tails = [
+            shifted_splits[0].1,
+            shifted_splits[1].1,
+            shifted_splits[2].1,
+        ];
+
+        let shifted_heads_iterator = transpose_heads::<S>(shifted_heads);
         // the tail is the remainder left after allocating v into simd vectors
         // len(tail) = len(v) % simd_vector_length
         let (out_head, out_tail) = S::as_mut_simd_f32s(self.output);
 
-        for (i, column) in shifted_heads_t.into_iter().enumerate() {
-
-            for (x, k) in column.iter().zip(kernel_simd.iter()) {
-                out_head[i] = simd.mul_add_f32s(*x, *k, out_head[i]);
-            }
+        for (i, column) in shifted_heads_iterator.enumerate() {
+            out_head[i] = simd.mul_add_f32s(column[0], kernel_simd[0], out_head[i]);
+            out_head[i] = simd.mul_add_f32s(column[1], kernel_simd[1], out_head[i]);
+            out_head[i] = simd.mul_add_f32s(column[2], kernel_simd[2], out_head[i]);
             out_head[i] = simd.mul_f32s(out_head[i], kernel_len_recip_simd);
         }
 
         // now we need to handle the tails
-    //     let tail_len = shifted_tails[0].len();
-    //     if tail_len > 0 {
+        // can i naive iterate on the tails or is that too slow?
 
-    //         for (x,k) in shifted_tails.iter().zip(kernel_simd.iter()) {
-    //             out_tail = simd.mul_add_f32s(
-    //                 simd.partial_load_f64s(x), 
-    //                 *k, out_tail)
-    //         }
-    //         acc0 =
-    //             simd.mul_add_f32s(simd.partial_load_f32s(x1), simd.partial_load_f32s(y1), acc0);
-    //     }
-    }
-}
-fn transpose_heads<S: Simd>(simd: S, heads: Vec<&[S::f32s]>) -> Vec<Vec<S::f32s>> {
-    let mut transposed = vec![vec![simd.splat_f32s(0.0); heads.len()]; heads[0].len()];
-    for i in 0..heads.len() {
-        for j in 0..heads[0].len() {
-            transposed[j][i] = heads[i][j];
+        if out_tail.len() > 0 {
+            for i in 0..out_tail.len() - 2 {
+                out_tail[i] = shifted_tails[0][i] * self.kernel[0]
+                    + shifted_tails[1][i] * self.kernel[1]
+                    + shifted_tails[2][i] * self.kernel[2];
+                out_tail[i] *= KERNEL_RECIP_3;
+            }
         }
     }
-    transposed
 }
 
-fn create_shifted_copies<'a, T: Clone>(v: &[T], num_shifts: &usize, pad_value: &T) -> Vec<Vec<T>> {
-
-    let shifted_copies = (0..*num_shifts).map(|shift| {
-        v.iter().skip(shift).chain(std::iter::repeat(pad_value).take(shift)).cloned().collect()
-    }).collect::<Vec<_>>();
-
-
-    // let mut shifted_copies = vec![vec![pad_value.clone(); v.len()]; num_shifts];
-    // for shift in 0..num_shifts {
-    //     for i in 0..v.len() {
-    //         if i + shift < v.len() {
-    //             shifted_copies[shift][i] = v[i+shift].clone();
-    //         }
-    //     }
-    // }
-    shifted_copies
-}
-
-fn naive_convolve(input: &[f32], kernel: &[f32]) -> Vec<f32> {
-    let kernel_len = kernel.len();
-    let kernel_len_f32 = kernel_len as f32;
-    let output_len = input.len() - kernel_len + 1;
-    let mut output = Vec::with_capacity(output_len);
-    for i in 0..output_len {
-        let mut acc = 0.0;
-        for j in 0..kernel_len {
-            acc += input[i + j] * kernel[j];
-        }
-        output.push(acc/ kernel_len_f32);
+fn transpose_heads<S: Simd>(heads: [&[S::f32s]; 3]) -> TransposedHeadsIter<S> {
+    TransposedHeadsIter {
+        heads,
+        current_idx: 0,
     }
-    output
 }
-fn main() {
-    let v = [1.0, 2.0, 3.0,4.0];// 4.0, 5.0,12.0,98.0,23.6,101.7];
-    let k = [1.0, 2.0 ,3.0,7.0];
-    // let outs = naive_convolve(&v, &k);
-    // dbg!("{:?}", &outs);
-    // let known_result = [14.0, 20.0, 26.0];
-    // for (a, b) in outs.iter().zip(known_result.iter()) {
-    //     assert!((a - b).abs() < 1e-6);
-    // }
-    let mut output = vec![0.0; v.len()];
 
-    let convolver = Convolver {
-        input: &v,
-        kernel: &k,
-        output: &mut output,
-    };
+fn create_3_shifted_copies<'a, T: Clone>(v: &'a [T]) -> [&'a [T]; 3] {
+    [&v[0..v.len() - 2], &v[1..v.len() - 1], &v[2..v.len()]]
+}
+
+fn naive_convolve_3k(input: &[f32], kernel: &[f32], output: &mut [f32]) {
+    assert_eq!(kernel.len(), 3);
+    let output_len = output.len();
+    for (i, out_element) in output.iter_mut().take(output_len - 2).enumerate() {
+        *out_element =
+            (input[i] * kernel[0] + input[i + 1] * kernel[1] + input[i + 2] * kernel[2]) / 3.0;
+    }
+    *output.last_mut().unwrap() = 0.0;
+    return;
+}
+
+fn bench_naive_convolve_3k(
+    bencher: Bencher,
+    PlotArg(n): PlotArg
+) {
+    let v = &*vec![1.0; n];
+    let k = &[1.0, 2.0, 3.0];
+    let output = &mut *vec![0.0; n];
+
+    bencher.bench(|| naive_convolve_3k(v, k, output));
+}
+
+fn bench_simd_3k(
+    bencher: Bencher,
+    PlotArg(n): PlotArg
+) {
+    let v = &*avec![1.0; n];
+    let k = &[1.0, 2.0, 3.0];
+    let output = &mut *avec![0.0; n];
+
+    
     let arch = Arch::new();
-    arch.dispatch(convolver);
+    bencher.bench(|| arch.dispatch(Convolver3k {
+        input: v,
+        kernel: k,
+        output,
+    }));
+}
 
-    dbg!(&output);
+fn main() -> std::io::Result<()> {
+    let mut bench = Bench::new(BenchConfig::from_args()?);
 
-    // do naive
-    let outs = naive_convolve(&v, &k);
-    dbg!("{:?}", &outs);
+    let mut params = vec![];
+    for n in 5..10 {
+        params.push(10 * n);
+    }
+
+    for n in 5..10 {
+        params.push(100 * n);
+    }
+    for n in 5..10 {
+        params.push(1000 * n);
+    }
+
+    bench.register_many(list![
+        bench_naive_convolve_3k,
+        bench_simd_3k,
+    ], params.iter().copied().map(PlotArg));
+
+    bench.run()?;
+    Ok(())
 
 }
